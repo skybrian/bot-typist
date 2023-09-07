4df5b356a7ffc0eaca9822f9f7b7675f255e1ce0
Brian Slesinsky <brian-git@slesinsky.org>
2023-09-06 23:29:41 -0700

    a horizontal rule marks the start of the prompt

--- src/lib/botrequest.ts
+++ src/lib/botrequest.ts
@@ -1,7 +1,39 @@
+import { text } from "stream/consumers";
+
 interface Cell {
   languageId: string;
   text: string;
 }
+
+const isHorizontalRule = (line: string): boolean => {
+  return /^[-*_]{3,}$/.test(line.trim());
+};
+
+type CellParse = ["start", string] | ["text", string];
+
+const parse = (text: string): CellParse => {
+  let foundStart = false;
+  let result = "";
+
+  let firstLine = true;
+  for (const line of text.split("\n")) {
+    if (!firstLine) {
+      result += "\n";
+    }
+
+    if (isHorizontalRule(line)) {
+      foundStart = true;
+      result = "";
+      firstLine = true;
+      continue;
+    }
+
+    result += line;
+    firstLine = false;
+  }
+  const tag = foundStart ? "start" : "text";
+  return [tag, result];
+};
 
 /**
  * Determines the prompt to use, given the current cell.
@@ -11,10 +43,26 @@ export function chooseBotPrompt(
   cellIndex: number,
 ): string {
   let prompt = "";
+
+  const pushCell = (languageId: string, text: string) => {
+    prompt += `%${languageId}\n${text}\n`;
+  };
+
   for (let i = 0; i <= cellIndex; i++) {
     const cell = cellAt(i);
-    if (cell.text.trim() !== "") {
-      prompt += `%${cell.languageId}\n${cell.text}\n`;
+    if (cell.text.trim() === "") {
+      continue;
+    } else if (cell.languageId !== "markdown") {
+      pushCell(cell.languageId, cell.text);
+      continue;
+    }
+
+    const parsed = parse(cellAt(i).text);
+    if (parsed[0] === "start") {
+      prompt = "";
+    }
+    if (parsed[1].trim() !== "") {
+      pushCell(cell.languageId, parsed[1]);
     }
   }
 
--- src/test/node/botrequest.test.ts
+++ src/test/node/botrequest.test.ts
@@ -4,30 +4,32 @@ import * as fc from "fast-check";
 import { chooseBotPrompt } from "../../lib/botrequest";
 
 describe("chooseBotPrompt", () => {
-  const checkEmptyPrompt = (text: string) => {
-    const cells = [{ languageId: "markdown", text }];
+  const checkEmptyPrompt = ([languageId, text]: [string, string]) => {
+    const cells = [{ languageId: languageId, text }];
     const cellAt = (idx: number) => cells[idx];
     const prompt = chooseBotPrompt(cellAt, 0);
     expect(prompt).toEqual("");
   };
 
   it("returns an empty string when given an empty cell", () => {
-    checkEmptyPrompt("");
+    checkEmptyPrompt(["markdown", ""]);
+    checkEmptyPrompt(["python", ""]);
   });
 
   it("returns an empty string for a blank cell", () => {
-    const args = fc.stringOf(fc.constantFrom(" ", "\t", "\n", "\r"), {
+    const blank = fc.stringOf(fc.constantFrom(" ", "\t", "\n", "\r"), {
       minLength: 1,
     });
+    const args = fc.tuple(fc.constantFrom("markdown", "python"), blank);
     fc.assert(fc.property(args, checkEmptyPrompt));
   });
 
-  it("returns the text of a non-empty cell", () => {
-    const args = fc.unicodeString({ minLength: 1 }).filter((s) =>
-      s.trim() !== ""
-    );
+  const nonEmptyText = fc.unicodeString({ minLength: 1 }).filter((s) =>
+    s.trim() !== ""
+  );
 
-    fc.assert(fc.property(args, (text) => {
+  it("returns the text of a non-empty cell", () => {
+    fc.assert(fc.property(nonEmptyText, (text) => {
       const cells = [{ languageId: "markdown", text }];
       const cellAt = (idx: number) => cells[idx];
       const prompt = chooseBotPrompt(cellAt, 0);
@@ -35,15 +37,26 @@ describe("chooseBotPrompt", () => {
     }));
   });
 
+  it("doesn't include text after the active cell index", () => {
+    const cells = [
+      { languageId: "markdown", text: "first" },
+      { languageId: "markdown", text: "second" },
+    ];
+    const cellAt = (idx: number) => cells[idx];
+    const prompt = chooseBotPrompt(cellAt, 0);
+    expect(prompt).toEqual(`%markdown\nfirst\n`);
+  });
+
   const anyCue = fc.constantFrom("bot: ", "q: ", "1: ", "gpt4: ");
 
   const anyCueCell = fc.tuple(anyCue, fc.unicodeString({}))
     .map(([cue, text]) => ({ languageId: "markdown", text: cue + text }));
 
-  const anyPythonCell = fc.unicodeString({ minLength: 1 }).map((text) => ({
-    languageId: "python",
-    text,
-  }));
+  const anyPythonCell = fc.unicodeString({ minLength: 1 })
+    .filter((s) => s.trim() !== "").map((text) => ({
+      languageId: "python",
+      text,
+    }));
 
   it("returns the text of a conversation", () => {
     const args = fc.array(fc.oneof(anyCueCell, anyPythonCell), {
@@ -57,6 +70,63 @@ describe("chooseBotPrompt", () => {
         .map((cell) => `%${cell.languageId}\n${cell.text}\n`)
         .join("");
       expect(prompt).toEqual(expected);
+    }));
+  });
+
+  const anyHorizontalRule = fc.stringOf(fc.constantFrom("-", "*", "_"), {
+    minLength: 3,
+    maxLength: 10,
+  });
+
+  it("doesn't include text before a horizontal rule", () => {
+    const textBefore = fc.unicodeString({ minLength: 1 }).map((text) =>
+      text + "\n\n"
+    );
+
+    const textAfter = fc.unicodeString({ minLength: 1 }).map((text) =>
+      "\n" + text
+    );
+
+    const args = fc.tuple(textBefore, anyHorizontalRule, textAfter).map((
+      [before, hr, after],
+    ) => ({
+      input: { languageId: "markdown", text: before + hr + "\n" + after },
+      expected: after.trim() === "" ? "" : "%markdown\n" + after + "\n",
+    }));
+
+    fc.assert(fc.property(args, ({ input, expected }) => {
+      const cells = [input];
+      const cellAt = (idx: number) => cells[idx];
+      const prompt = chooseBotPrompt(cellAt, 0);
+      expect(prompt).toEqual(expected);
+    }));
+  });
+
+  const hrCell = anyHorizontalRule.map((hr) => ({
+    languageId: "markdown",
+    text: hr,
+  }));
+
+  it("returns an empty string if there's nothing after a horizontal rule", () => {
+    fc.assert(fc.property(hrCell.map((c) => [c]), (cells) => {
+      const cellAt = (idx: number) => cells[idx];
+      const prompt = chooseBotPrompt(cellAt, cells.length - 1);
+      expect(prompt).toEqual("");
+    }));
+  });
+
+  it("returns text from cells after a horizontal rule", () => {
+    const args = fc.tuple(
+      fc.oneof(anyCueCell, anyPythonCell),
+      hrCell,
+      fc.oneof(anyCueCell, anyPythonCell),
+    );
+
+    fc.assert(fc.property(args, (cells) => {
+      const cellAt = (idx: number) => cells[idx];
+      const prompt = chooseBotPrompt(cellAt, cells.length - 1);
+      const after = cells[2];
+      expect(prompt).toEqual(`%${after.languageId}\n${after.text}\n`);
     }));
   });
 });
